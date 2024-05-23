f6b9df8a4193b6776142a0a28f5138a36e69041e
Parsa <parsaalizadeh2022@gmail.com>
2024-05-23 22:27:10 +0430

    version2

--- articles/admin.py
+++ articles/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
-from .models import Articles_page
+from .models import Articles_page,Category_Articles
 
 
 @admin.register(Articles_page)
 class Articles_Admin(admin.ModelAdmin):
+    pass
+@admin.register(Category_Articles)
+class Category_admin(admin.ModelAdmin):
     pass
--- articles/models.py
+++ articles/models.py
@@ -1,14 +1,23 @@
 from django.db import models
 
+choices_custom=[
+    ("d","draft"),
+    ("p","published"),
+]
 class Articles_page(models.Model):
     title=models.CharField(max_length=100)
     description=models.TextField()
     image=models.ImageField(upload_to='media/images/')
     author_name=models.CharField(max_length=100)
     category=models.ManyToManyField("Category_Articles")
+    published=models.CharField(max_length=100,choices=choices_custom,default="p")
     def __str__(self):
         return self.title
     
 
 class Category_Articles(models.Model):
     name=models.CharField(max_length=100)
+    published=models.BooleanField(default=True)
+    def __str__(self):
+        return self.name
+    
